c7b35f2e5160488c9d41cbc0cfe7d002da6fe34e
Peter Ilberg <peter.ilberg@gmail.com>
2025-11-13 13:31:58 +0100

    Add accumulator example.

--- examples/accumulator.ml
+++ examples/accumulator.ml
@@ -1,97 +1,87 @@
-(*
+module StateMachine : sig
+  type ('input, 'output, 'state) t
 
-module type STATEMACHINE = sig
-  type t
-  type state
-  type input
-  type output
+  val make_with
+    :  get_start_state:(unit -> 'state)
+    -> get_next_state:('state -> 'input -> 'state * 'output)
+    -> ('input, 'output, 'state) t
 
-  val get_start_state : machine:t -> state
-  val get_next_state : machine:t -> state:state -> input:input -> state * output
+  val trace
+    :  trace_start:('state -> unit)
+    -> trace_step:(int -> 'input -> 'output -> 'state -> unit)
+    -> machine:('input, 'output, 'state) t
+    -> inputs:'input list
+    -> 'output list
+end = struct
+  type ('input, 'output, 'state) t =
+    { get_start_state : unit -> 'state
+    ; get_next_state : 'state -> 'input -> 'state * 'output
+    }
+
+  let make_with ~get_start_state ~get_next_state = { get_start_state; get_next_state }
+
+  let trace ~trace_start ~trace_step ~machine ~inputs =
+    let state = ref (machine.get_start_state ()) in
+    let step i =
+      let s', o = machine.get_next_state !state i in
+      state := s';
+      o
+    in
+    trace_start !state;
+    inputs
+    |> List.mapi (fun i input ->
+      let o = step input in
+      trace_step i input o !state;
+      o)
+  ;;
 end
 
-module Accumulator : STATEMACHINE = struct
-  type t = { initial_value : int }
-  type input = int
-  type output = int
-  type state = int
-
-  let make ~initial_value = { initial_value }
-  let get_start_state ~machine = machine.initial_value
-  let get_next_state ~machine:_ ~state ~input = (state + input, state + input)
-end
-*)
-
-(*
-module StateMachine (SM: STATEMACHINE) = struct
-  type inputs = SM.input list [@@deriving.show]
-  
-  let trace ~machine:SM.t ~inputs:inputs=
-    ()
-end
-
-  Module type SM = sig
-	type state
-	type input
-	type output
-	get start state
-	get next state
-End
-
-Let accumulator (type a) (module A : Addable with type t = a) ~initial:value:a =
-	(module struct
-		type state = A.t
-		type input = int
-		type output = int
-		let get_start_state = value
-		let get_next_state ~state:state ~input:input = (state + input, ...)
-	end : SM with type state = int, input = A.t, ...)
-
-Let trace ~machine:(module M : StateMachine) =
-	let (state', output) M.get_next_state ~state:state ~input:input in
-	...
-
-*)
-
-(*
-let step ~stepper:{mut state:int} ~input:int =
-  
-
-let trace ~machine ~inputs =
-  let stepper = { mut state = Accumulator.get_start_state machine} in
-  inputs |> List.map (fun i -> step stepper i)
-*)
-(*
-module type Addable = sig
+module type Accumulable = sig
   type t
 
-  val add : t -> t -> t
+  val accumulate : t -> t -> t
 end
-type t = { initial_value : int }
 
-let make ~initial_value:value = { initial_value = value }
-*)
+module AccumulatorMake (A : Accumulable) = struct
+  let make_with ~initial_value =
+    let get_start_state () = initial_value in
+    let get_next_state s i =
+      let sum = A.accumulate s i in
+      sum, sum
+    in
+    StateMachine.make_with ~get_start_state ~get_next_state
+  ;;
+end
 
-let get_start_state accumulator = accumulator
-let get_next_state _accumulator state input = (state + input, state + input)
+module AccumulableInt : Accumulable with type t = int = struct
+  type t = int
 
-let stepper accumulator initial =
-  let state = ref initial in
-  fun i input ->
-    let s, o = get_next_state accumulator !state input in
-    Format.printf "%d: input %d produces %d with new state: %d\n" i input o s;
-    state := s;
-    o
+  let accumulate = Int.add
+end
 
-let trace accumulator inputs =
-  let state = get_start_state accumulator in
-  Format.printf "Start state: %d\n" state;
-  inputs |> List.mapi (stepper accumulator state)
+module AccumulatorInt = AccumulatorMake (AccumulableInt)
 
+type input = int [@@deriving show]
+type output = int [@@deriving show]
+type state = int [@@deriving show]
 type outputs = int list [@@deriving show]
 
+let trace =
+  let trace_start state = Printf.printf "Start state: %s\n" (show_state state) in
+  let trace_step i input output state =
+    Printf.printf
+      "%d: input %s produces %s with new state: %s\n"
+      i
+      (show_input input)
+      (show_output output)
+      (show_state state)
+  in
+  StateMachine.trace ~trace_start ~trace_step
+;;
+
 let () =
-  let accumulator = 0 in
+  let accumulator = AccumulatorInt.make_with ~initial_value:0 in
   let inputs = [ 100; -3; 4; -123; 10 ] in
-  let outputs = trace accumulator inputs in
-  Format.printf "Output: %s" (show_outputs outputs)
+  let outputs = trace ~machine:accumulator ~inputs in
+  Printf.printf "Output: %s" (show_outputs outputs)
+;;
