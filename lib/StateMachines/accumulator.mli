5966cab1ea1b6ff7cc667395a0f3e0a92db56d0f
Peter Ilberg <peter.ilberg@gmail.com>
2025-11-25 15:53:51 +0100

    Remove old accumulator modules.

--- lib/StateMachines/accumulator.mli
+++
@@ -1,9 +0,0 @@
-module type Accumulable = sig
-  type t
-
-  val accumulate : t -> t -> t
-end
-
-module Make (A : Accumulable) : sig
-  val make_with : initial_value:A.t -> (A.t, A.t, A.t) StateMachine.t
-end
